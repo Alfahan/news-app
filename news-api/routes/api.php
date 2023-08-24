36966b1dff8b469b5e3f6232a4fc6eb0b60ecd06
telkomdev-alfahan <ali.farhan160@gmail.com>
2023-08-24 08:56:38 +0700

    feat: Restfil API Dashboard

--- news-api/routes/api.php
+++ news-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Admin\DashboardController;
 use App\Http\Controllers\Api\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,7 +19,17 @@ use Illuminate\Support\Facades\Route;
 // route login
 Route::post('/login', [LoginController::class, 'index']);
 
+//group route with middleware "auth"
 Route::group(['middleware' => 'auth:api'], function() {
     // logout
     Route::post('/logout', [LoginController::class, 'logout']);
+});
+
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+    //group route with middleware "auth:api"
+    Route::group(['middleware' => 'auth:api'], function () {
+        //dashboard
+        Route::get('/dashboard', DashboardController::class);
+    });
 });
